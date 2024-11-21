ecb9282af55c708e7d4c42ad755e6b70002291cc
Jan Wilmans <jan.wilmans@gmail.com>
2024-11-21 21:32:08 +0100

    added start.bat

---
+++ start.bat
@@ -0,0 +1,4 @@
+:: usage python scripts/make.py <qt/cmake>
+:: where qt/cmake is the directory in which 'Qt6\Qt6Config.cmake' can be found
+
+python scripts/make.py C:\Qt\6.7.2\msvc2019_64\lib\cmake
